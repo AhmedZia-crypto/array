2bde857c02abb203bd868885ace1cf9ca5bf5df4
AhmedZia-crypto <zia14fox@gmail.com>
2025-10-17 13:39:38 +0300

    Add files via upload

---
+++ array1.py
@@ -0,0 +1,24 @@
+import array
+
+number = array.array("i", [1,2,3,4,5]) # i means integers
+print(number)
+
+for i in range(0,3):
+    print("b"[i], end =" ")
+
+a.insert(1, 4)
+
+print("\nArray after insertion : ", end = " ")
+for i in (a):
+    print(i, end = " ")
+print()
+
+b.append(4.4)
+
+print("\nArray after insertion : ", end = " ")
+for i in (b):
+    print(i, end = " ")
+print()
+
+array_num.count(3)
+array_num.reverse()
